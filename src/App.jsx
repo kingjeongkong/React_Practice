c5c91a6872b6b5203a9837d26e88ea25ca4cbc0c
kingjeongkong <wjdqls9223@gmail.com>
2024-10-20 16:44:06 +0900

    페이지 컴포넌트들 리액트를 사용하여 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
 };
 
 export default App;
