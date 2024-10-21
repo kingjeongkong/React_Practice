e89d558d60b8742034209fc8165c84d1d5a81818
kingjeongkong <wjdqls9223@gmail.com>
2024-10-21 18:12:09 +0900

    inline css stylesheet

--- basic-react/src/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Button.module.css';
-
-const Button = () => {
-  return <button className={styles.button}>Click Me</button>;
-};
-
-export default Button;
