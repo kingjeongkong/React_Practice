c9b8bd092bb98229b0902dfebaf9e670bb415b20
kingjeongkong <wjdqls9223@gmail.com>
2024-10-22 01:48:16 +0900

    render lists

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,11 +1,28 @@
-import UserGreeting from './UserGreeting.jsx';
+import List from './List';
 
 function App() {
+  const fruits = [
+    { id: 1, name: 'apple', calories: 95 },
+    { id: 2, name: 'orange', calories: 45 },
+    { id: 3, name: 'banana', calories: 105 },
+    { id: 4, name: 'coconut', calories: 159 },
+    { id: 5, name: 'pineapple', calories: 37 },
+  ];
+
+  const vegetables = [
+    { id: 6, name: 'potattoes', calories: 10 },
+    { id: 7, name: 'celery', calories: 15 },
+    { id: 8, name: 'carrots', calories: 25 },
+    { id: 9, name: 'corn', calories: 63 },
+    { id: 10, name: 'broccoli', calories: 50 },
+  ];
+
   return (
     <>
-      <UserGreeting isLoggedIn={true} username="JeongBeen" />
-      <UserGreeting isLoggedIn={false} username="JeongBeen" />
-      <UserGreeting />
+      {fruits.length > 0 ? <List items={fruits} category="Fruits" /> : null}
+      {vegetables.length > 0 ? (
+        <List items={vegetables} category="Vegetables" />
+      ) : null}
     </>
   );
 }
