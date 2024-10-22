45b1a83a1538b84b3d5272330de181704854748b
kingjeongkong <wjdqls9223@gmail.com>
2024-10-22 18:03:20 +0900

    color picker app practice

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+import ColorPicker from './ColorPicker';
 
 const App = () => {
-  return <MyComponent />;
+  return <ColorPicker />;
 };
 
 export default App;
