4918aea58a7b68341493813419add61a70fe19e0
kingjeongkong <wjdqls9223@gmail.com>
2024-10-25 02:53:46 +0900

    React Router 기본 설정

--- react-jobs-website/src/components/HomeCards.jsx
+++ react-jobs-website/src/components/HomeCards.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from './Card';
 
 const HomeCards = () => {
@@ -8,27 +9,23 @@ const HomeCards = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
             <h2 className="text-2xl font-bold">For Developers</h2>
-            <p className="mt-2 mb-4">
-              Browse our React jobs and start your career today
-            </p>
-            <a
-              href="/jobs.html"
+            <p className="mt-2 mb-4">Browse our React jobs and start your career today</p>
+            <Link
+              to="/jobs"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
-            </a>
+            </Link>
           </Card>
           <Card bg="bg-indigo-100">
             <h2 className="text-2xl font-bold">For Employers</h2>
-            <p className="mt-2 mb-4">
-              List your job to find the perfect developer for the role
-            </p>
-            <a
-              href="/add-job.html"
+            <p className="mt-2 mb-4">List your job to find the perfect developer for the role</p>
+            <Link
+              to="/add-job"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
-            </a>
+            </Link>
           </Card>
         </div>
       </div>
--- react-jobs-website/src/components/JobListings.jsx
+++ react-jobs-website/src/components/JobListings.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import jobs from '../../jobs.json';
+import jobs from '../../jobsJSON.json';
 import JobListing from './JobListing';
 
 const JobListings = () => {
@@ -8,9 +8,7 @@ const JobListings = () => {
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
-        </h2>
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">Browse Jobs</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {recentJobs.map((job) => (
             <JobListing key={job.id} job={job} />
