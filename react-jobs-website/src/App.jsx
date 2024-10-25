895ff8ab327be01a912c67e076f5189efd1b06e1
kingjeongkong <wjdqls9223@gmail.com>
2024-10-26 01:07:07 +0900

    useParams를 활용한 JobPage 구현

--- react-jobs-website/src/App.jsx
+++ react-jobs-website/src/App.jsx
@@ -3,18 +3,20 @@ import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider,
+  RouterProvider
 } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/Jobpage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
