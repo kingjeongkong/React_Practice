9e5468dfd897c120a450eca3188942c4c31517fa
kingjeongkong <wjdqls9223@gmail.com>
2024-10-26 01:16:41 +0900

    React Data Loader(useLoaderData)를 활용한 JobPage Data Load

--- react-jobs-website/src/pages/JobPage.jsx
+++ react-jobs-website/src/pages/JobPage.jsx
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Spinner from '../components/Spinner';
+import React from 'react';
+import { useParams, useLoaderData } from 'react-router-dom';
 
 const JobPage = () => {
   const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const job = useLoaderData();
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const response = await fetch(`/api/jobs/${id}`);
-        const data = await response.json();
-        setJob(data);
-      } catch (error) {
-        console.log('Error fetching data', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchJob();
-  }, []);
-
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return <h1>{job.title}</h1>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const response = await fetch(`/api/jobs/${params.id}`);
+  const data = await response.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
